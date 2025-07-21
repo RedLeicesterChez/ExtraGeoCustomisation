3f87d2af53077ec3b3d0d8a70451312869cac1c6
Red Leicester Cheese <red_leicester_cheese@outlook.com>
2025-07-21 17:19:31 +0100

    Actually add important loggers (god im stupid)

--- ExtraGeoCustomisation/Utils/LogEGC.cs
+++ ExtraGeoCustomisation/Utils/LogEGC.cs
@@ -15,21 +15,95 @@ namespace ExtraGeoCustomisation.Utils
         }
 
         private static string Format(object msg) => msg.ToString();
-        public static void Info(BepInExInfoLogInterpolatedStringHandler handler) => _Logger.LogInfo(handler);
-        public static void Info(string str) => _Logger.LogMessage(str);
-        public static void Info(object data) => _Logger.LogMessage(Format(data));
-        public static void Debug(BepInExDebugLogInterpolatedStringHandler handler) => _Logger.LogDebug(handler);
-        public static void Debug(string str) => _Logger.LogDebug(str);
-        public static void Debug(object data) => _Logger.LogDebug(Format(data));
-        public static void Error(BepInExErrorLogInterpolatedStringHandler handler) => _Logger.LogError(handler);
-        public static void Error(string str) => _Logger.LogError(str);
-        public static void Error(object data) => _Logger.LogError(Format(data));
-        public static void Fatal(BepInExFatalLogInterpolatedStringHandler handler) => _Logger.LogFatal(handler);
-        public static void Fatal(string str) => _Logger.LogFatal(str);
-        public static void Fatal(object data) => _Logger.LogFatal(Format(data));
-        public static void Warn(BepInExWarningLogInterpolatedStringHandler handler) => _Logger.LogWarning(handler);
-        public static void Warn(string str) => _Logger.LogWarning(str);
-        public static void Warn(object data) => _Logger.LogWarning(Format(data));
+        public static void Info(BepInExInfoLogInterpolatedStringHandler handler)
+        {
+            if (GlobalConfig.EnableDevLogs)
+                _Logger.LogInfo(handler);
+        }
+
+        public static void Info(string str)
+        {
+            if (GlobalConfig.EnableDevLogs)
+                _Logger.LogMessage(str);
+        }
+
+        public static void Info(object data)
+        {
+            if (GlobalConfig.EnableDevLogs)
+                _Logger.LogMessage(Format(data));
+        }
+
+        public static void Debug(BepInExDebugLogInterpolatedStringHandler handler)
+        {
+            if (GlobalConfig.EnableDevLogs)
+                _Logger.LogDebug(handler);
+        }
+
+        public static void Debug(string str)
+        {
+            if (GlobalConfig.EnableDevLogs)
+                _Logger.LogDebug(str);
+        }
+
+        public static void Debug(object data)
+        {
+            if (GlobalConfig.EnableDevLogs)
+                _Logger.LogDebug(Format(data));
+        }
+
+        public static void Error(BepInExErrorLogInterpolatedStringHandler handler)
+        {
+            if (GlobalConfig.EnableDevLogs)
+                _Logger.LogError(handler);
+        }
+
+        public static void Error(string str)
+        {
+            if (GlobalConfig.EnableDevLogs)
+                _Logger.LogError(str);
+        }
+
+        public static void Error(object data)
+        {
+            if (GlobalConfig.EnableDevLogs)
+                _Logger.LogError(Format(data));
+        }
+
+        public static void Fatal(BepInExFatalLogInterpolatedStringHandler handler)
+        {
+            if (GlobalConfig.EnableDevLogs)
+                _Logger.LogFatal(handler);
+        }
+
+        public static void Fatal(string str)
+        {
+            if (GlobalConfig.EnableDevLogs)
+                _Logger.LogFatal(str);
+        }
+
+        public static void Fatal(object data)
+        {
+            if (GlobalConfig.EnableDevLogs)
+                _Logger.LogFatal(Format(data));
+        }
+
+        public static void Warn(BepInExWarningLogInterpolatedStringHandler handler)
+        {
+            if (GlobalConfig.EnableDevLogs)
+                _Logger.LogWarning(handler);
+        }
+
+        public static void Warn(string str)
+        {
+            if (GlobalConfig.EnableDevLogs)
+                _Logger.LogWarning(str);
+        }
+
+        public static void Warn(object data)
+        {
+            if (GlobalConfig.EnableDevLogs)
+                _Logger.LogWarning(Format(data));
+        }
 
         public static void InfoImportant(BepInExInfoLogInterpolatedStringHandler handler) => _Logger.LogInfo(handler);
         public static void InfoImportant(string str) => _Logger.LogMessage(str);
