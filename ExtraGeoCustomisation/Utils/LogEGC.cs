ef5393d27d3e710ee104fe1e2e9a3d0bbdfdb433
Red Leicester Cheese <red_leicester_cheese@outlook.com>
2025-07-21 17:17:17 +0100

    Add important logs

--- ExtraGeoCustomisation/Utils/LogEGC.cs
+++ ExtraGeoCustomisation/Utils/LogEGC.cs
@@ -31,6 +31,22 @@ namespace ExtraGeoCustomisation.Utils
         public static void Warn(string str) => _Logger.LogWarning(str);
         public static void Warn(object data) => _Logger.LogWarning(Format(data));
 
+        public static void InfoImportant(BepInExInfoLogInterpolatedStringHandler handler) => _Logger.LogInfo(handler);
+        public static void InfoImportant(string str) => _Logger.LogMessage(str);
+        public static void InfoImportant(object data) => _Logger.LogMessage(Format(data));
+        public static void DebugImportant(BepInExDebugLogInterpolatedStringHandler handler) => _Logger.LogDebug(handler);
+        public static void DebugImportant(string str) => _Logger.LogDebug(str);
+        public static void DebugImportant(object data) => _Logger.LogDebug(Format(data));
+        public static void ErrorImportant(BepInExErrorLogInterpolatedStringHandler handler) => _Logger.LogError(handler);
+        public static void ErrorImportant(string str) => _Logger.LogError(str);
+        public static void ErrorImportant(object data) => _Logger.LogError(Format(data));
+        public static void FatalImportant(BepInExFatalLogInterpolatedStringHandler handler) => _Logger.LogFatal(handler);
+        public static void FatalImportant(string str) => _Logger.LogFatal(str);
+        public static void FatalImportant(object data) => _Logger.LogFatal(Format(data));
+        public static void WarnImportant(BepInExWarningLogInterpolatedStringHandler handler) => _Logger.LogWarning(handler);
+        public static void WarnImportant(string str) => _Logger.LogWarning(str);
+        public static void WarnImportant(object data) => _Logger.LogWarning(Format(data));
+
         [Conditional("DEBUG")]
         public static void DebugOnly(object data)
         {
